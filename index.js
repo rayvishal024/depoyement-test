fc8c2d6180d85053951ce8c35ad8eb5ba56555b5
rayvishal <rayvishal2005@gmail.com>
2026-06-07 14:49:26 +0530

    chore: Correct casing in response message for consistency

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello from Production! with ci-cd' });
+    res.json({ message: 'Hello from Production! with CI/CD' });
 });
 
 app.get('/health', (req, res) => {
